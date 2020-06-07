fdac3ee98002f6eddfab2200fc1e601bc398b79e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-07 01:26:38 +0000

    Deploy AztianPahola/Learning-React to github.com/AztianPahola/Learning-React.git:gh-pages

---
+++ precache-manifest.26708246ce006c31f0391249df3c10d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30630222ee4ac54c3a281328f458185",
+    "url": "/Learning-React/index.html"
+  },
+  {
+    "revision": "3faf05c2b10bebf68b44",
+    "url": "/Learning-React/static/css/main.1c03a0e6.chunk.css"
+  },
+  {
+    "revision": "4cd5e257fae04ed23f1e",
+    "url": "/Learning-React/static/js/2.290b5f04.chunk.js"
+  },
+  {
+    "revision": "0fce063a4ec3cb005e7265693b407036",
+    "url": "/Learning-React/static/js/2.290b5f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3faf05c2b10bebf68b44",
+    "url": "/Learning-React/static/js/main.9dabaece.chunk.js"
+  },
+  {
+    "revision": "e7b9a9b95824eca3ebca",
+    "url": "/Learning-React/static/js/runtime-main.db5802f1.js"
+  }
+]);
